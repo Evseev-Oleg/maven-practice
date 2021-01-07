1c440f89a3e29bf6bbabc1d328f0a31374e0ddea
Evseev Oleg <oleg063053@gmail.com>
2021-01-07 22:49:46 +0400

    Add practice maven

---
+++ src/main/java/ru/evseev/oleg/App.java
@@ -0,0 +1,15 @@
+package ru.evseev.oleg;
+
+
+/**
+ * Программа HelloWorld реализует приложение, которое
+ * выводит "Привет, Мир!" на стандартном потоке вывода.
+ */
+public class App 
+{
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
